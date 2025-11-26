79da9f37a476dee1e5f2fc84b77a1bbeb07ce4e0
Claudinei <claudineisilva.ti@fundecc.org.br>
2025-11-26 17:38:00 -0300

    feat(db): Configuração do mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // torna o .env disponível em toda a aplicação
     }),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
   ],
   controllers: [AppController],
   providers: [AppService],
